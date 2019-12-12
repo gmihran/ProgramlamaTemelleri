47507da3c35e67f20ea1f9989b5544ef4fe149b7
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2019-12-12 13:55:30 +0300

    Add files via upload

---
+++ Kodlama/Hafta11/sos_oyunu.cpp
@@ -0,0 +1,79 @@
+#include "pch.h"
+#include <iostream>
+#include <string>
+//Bu örnek Yapay Zeka ile çözülebilir
+//Bilgisayar SOS yazmaya çalýþabilir veya kullanýcýnýn yazmamasý için gerekli yerleri doldurabilir.
+using namespace std;
+char sos[3][3] = { '#','#','#','#','#','#','#','#','#' }, pc_deger;
+int i, j;
+bool kontrol = false;
+void yazdir(){
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			cout << sos[i][j];
+		}
+		cout << endl;
+	}
+}
+void pc() {
+	//Eðer # kalmamýþsa bu fonksiyon çaðrýlmamalýdýr
+	char deger[2] = { 'S','O' };
+	int x = rand() % 2 + 0;
+	pc_deger = deger[x];
+	int satir, sutun;
+	do {
+		satir = rand() % 3 + 0;
+		sutun = rand() % 3 + 0;
+	} while (sos[satir][sutun]!='#');
+	sos[satir][sutun] = pc_deger;
+}
+
+void durum() {
+	int sayac = 0;
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			if (sos[i][j] == '#') {
+				sayac++;
+			}
+		}
+	}
+	if (sayac == 0)
+		kontrol = true; //oyun bitti
+}
+void kullanici() {
+	int satir, sutun;
+	char deger;
+	do{
+	cout << "Satýr deðeri:";
+	cin >> satir;
+	cout << "Sütun deðeri:";
+	cin >> sutun;
+	} while (sos[satir-1][sutun-1] != '#');
+	cout << "S-O :";
+	cin >> deger;
+	//Burada S-O doðru girildi mi? Kontrol edilecek.
+	sos[satir - 1][sutun - 1] = deger;
+}
+
+int main()
+{
+	
+	//sos oyunu
+	//Bilgisayar boþ olan alanlara rastgele s o s koyar
+	//Biz de onu yenmeye çalýþýrýz
+	
+	do{
+	yazdir();
+		pc();
+		kullanici();
+	} while (kontrol == false);
+	//else if () {
+		//SOS yazýldý mý? Kontrol edilecek
+
+	//}
+	cout << "Oyun bitti";
+		
+	
+	
+
+}
